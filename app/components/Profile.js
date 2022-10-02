f16e2f7524bc2b0f0b30b5c2604d9da0480a3f58
ruiac <rui.achada96@gmail.com>
2022-10-02 19:07:50 +0100

    section 10

--- app/components/Profile.js
+++ app/components/Profile.js
@@ -1,55 +1,146 @@
-import React, { useEffect, useContext, useState } from "react"
+import React, { useEffect, useContext } from "react"
 import Page from "./Page"
-import { useParams } from "react-router-dom"
+import { useParams, NavLink, Routes, Route } from "react-router-dom"
 import Axios from "axios"
 import StateContext from "../StateContext"
 import ProfilePosts from "./ProfilePosts"
+import ProfileFollowers from "./ProfileFollowers"
+import ProfileFollowing from "./ProfileFollowing"
+import { useImmer } from "use-immer"
 
 function Profile() {
-  const { username } = useParams() // http://localhost:3000/profile/user
+  const { username } = useParams()
   const appState = useContext(StateContext)
-  const [profileData, setProfileData] = useState({
-    profileUsername: "...",
-    profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
-    isFollowing: false,
-    counts: { postCounter: "", followerCounter: "", followingCount: "" } // default values to show before Axios completed the fetch
+  const [state, setState] = useImmer({
+    followActionLoading: false,
+    startFollowingRequestCount: 0,
+    stopFollowingRequestCount: 0,
+    profileData: {
+      profileUsername: "...",
+      profileAvatar: "https://gravatar.com/avatar/placeholder?s=128",
+      isFollowing: false,
+      counts: { postCount: "", followerCount: "", followingCount: "" }
+    }
   })
 
   useEffect(() => {
+    const ourRequest = Axios.CancelToken.source()
+
     async function fetchData() {
       try {
-        const response = await Axios.post(`/profile/${username}`, { token: appState.user.token })
-        console.log(response.data)
-        setProfileData(response.data)
+        const response = await Axios.post(`/profile/${username}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+        setState(draft => {
+          draft.profileData = response.data
+        })
       } catch (e) {
-        console.log(e)
+        console.log("There was a problem.")
       }
     }
     fetchData()
-  }, []) // run the function only the first time the component is rendered
+    return () => {
+      ourRequest.cancel()
+    }
+  }, [username])
+
+  useEffect(() => {
+    if (state.startFollowingRequestCount) {
+      setState(draft => {
+        draft.followActionLoading = true
+      })
+
+      const ourRequest = Axios.CancelToken.source()
+
+      async function fetchData() {
+        try {
+          const response = await Axios.post(`/addFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+          setState(draft => {
+            draft.profileData.isFollowing = true
+            draft.profileData.counts.followerCount++
+            draft.followActionLoading = false
+          })
+        } catch (e) {
+          console.log("There was a problem.")
+        }
+      }
+      fetchData()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.startFollowingRequestCount])
+
+  useEffect(() => {
+    if (state.stopFollowingRequestCount) {
+      setState(draft => {
+        draft.followActionLoading = true
+      })
+
+      const ourRequest = Axios.CancelToken.source()
+
+      async function fetchData() {
+        try {
+          const response = await Axios.post(`/removeFollow/${state.profileData.profileUsername}`, { token: appState.user.token }, { cancelToken: ourRequest.token })
+          setState(draft => {
+            draft.profileData.isFollowing = false
+            draft.profileData.counts.followerCount--
+            draft.followActionLoading = false
+          })
+        } catch (e) {
+          console.log("There was a problem.")
+        }
+      }
+      fetchData()
+      return () => {
+        ourRequest.cancel()
+      }
+    }
+  }, [state.stopFollowingRequestCount])
+
+  function startFollowing() {
+    setState(draft => {
+      draft.startFollowingRequestCount++
+    })
+  }
+
+  function stopFollowing() {
+    setState(draft => {
+      draft.stopFollowingRequestCount++
+    })
+  }
 
   return (
     <Page title="Profile Screen">
       <h2>
-        <img className="avatar-small" src={profileData.profileAvatar} /> {profileData.profileUsername}
-        <button className="btn btn-primary btn-sm ml-2">
-          Follow <i className="fas fa-user-plus"></i>
-        </button>
+        <img className="avatar-small" src={state.profileData.profileAvatar} /> {state.profileData.profileUsername}
+        {appState.loggedIn && !state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
+          <button onClick={startFollowing} disabled={state.followActionLoading} className="btn btn-primary btn-sm ml-2">
+            Follow <i className="fas fa-user-plus"></i>
+          </button>
+        )}
+        {appState.loggedIn && state.profileData.isFollowing && appState.user.username != state.profileData.profileUsername && state.profileData.profileUsername != "..." && (
+          <button onClick={stopFollowing} disabled={state.followActionLoading} className="btn btn-danger btn-sm ml-2">
+            Stop Following <i className="fas fa-user-times"></i>
+          </button>
+        )}
       </h2>
 
       <div className="profile-nav nav nav-tabs pt-2 mb-4">
-        <a href="#" className="active nav-item nav-link">
-          Posts: {profileData.counts.postCounter}
-        </a>
-        <a href="#" className="nav-item nav-link">
-          Followers: {profileData.counts.followerCounter}
-        </a>
-        <a href="#" className="nav-item nav-link">
-          Following: {profileData.counts.followingCount}
-        </a>
+        <NavLink to="" end className="nav-item nav-link">
+          Posts: {state.profileData.counts.postCount}
+        </NavLink>
+        <NavLink to="followers" className="nav-item nav-link">
+          Followers: {state.profileData.counts.followerCount}
+        </NavLink>
+        <NavLink to="following" className="nav-item nav-link">
+          Following: {state.profileData.counts.followingCount}
+        </NavLink>
       </div>
 
-      <ProfilePosts />
+      <Routes>
+        <Route path="" element={<ProfilePosts />} />
+        <Route path="followers" element={<ProfileFollowers />} />
+        <Route path="following" element={<ProfileFollowing />} />
+      </Routes>
     </Page>
   )
 }
--- app/components/Search.js
+++ app/components/Search.js
@@ -1,8 +1,9 @@
 import React, { useEffect, useContext } from "react"
 import DispatchContext from "../DispatchContext"
 import { useImmer } from "use-immer"
-import axios from "axios"
+import Axios from "axios"
 import { Link } from "react-router-dom"
+import Post from "./Post"
 
 function Search() {
   const appDispatch = useContext(DispatchContext)
@@ -10,52 +11,46 @@ function Search() {
   const [state, setState] = useImmer({
     searchTerm: "",
     results: [],
-    show: "neither", // can be loading icon or show the results
+    show: "neither",
     requestCount: 0
   })
 
   useEffect(() => {
     document.addEventListener("keyup", searchKeyPressHandler)
-    // clean up function. If search is closed we dont want to continue listening
     return () => document.removeEventListener("keyup", searchKeyPressHandler)
   }, [])
 
   useEffect(() => {
     if (state.searchTerm.trim()) {
-      // show the loading before sending the request
       setState(draft => {
         draft.show = "loading"
       })
       const delay = setTimeout(() => {
         setState(draft => {
-          draft.requestCount++ // increment request to have axios in a different useEffect
+          draft.requestCount++
         })
-      }, 700)
-      //clean up function to cancel time out (DEBOUNCE)
+      }, 750)
+
       return () => clearTimeout(delay)
     } else {
-      // if string is empty
       setState(draft => {
         draft.show = "neither"
       })
     }
   }, [state.searchTerm])
 
-  // NOTE: to search, our database should have title and body indexed
   useEffect(() => {
-    // needs to be > 0. Will not run when component first renders
     if (state.requestCount) {
-      // cancel token
-      const ourRequest = axios.CancelToken.source()
+      const ourRequest = Axios.CancelToken.source()
       async function fetchResults() {
         try {
-          const response = await axios.post("/search", { searchTerm: state.searchTerm }, { cancelToken: ourRequest.token })
+          const response = await Axios.post("/search", { searchTerm: state.searchTerm }, { cancelToken: ourRequest.token })
           setState(draft => {
             draft.results = response.data
             draft.show = "results"
           })
         } catch (e) {
-          console.log(e)
+          console.log("There was a problem or the request was cancelled.")
         }
       }
       fetchResults()
@@ -65,7 +60,6 @@ function Search() {
 
   function searchKeyPressHandler(e) {
     if (e.keyCode == 27) {
-      // 27 ESC key
       appDispatch({ type: "closeSearch" })
     }
   }
@@ -73,7 +67,6 @@ function Search() {
   function handleInput(e) {
     const value = e.target.value
     setState(draft => {
-      // usually we dont mutate state but with immer we can using this draft
       draft.searchTerm = value
     })
   }
@@ -102,30 +95,11 @@ function Search() {
                   <strong>Search Results</strong> ({state.results.length} {state.results.length > 1 ? "items" : "item"} found)
                 </div>
                 {state.results.map(post => {
-                  const date = new Date(post.createdDate)
-                  const dateFormatted = `${date.getMonth() + 1} / ${date.getDay()} / ${date.getFullYear()}`
-
-                  return (
-                    <Link
-                      onClick={() =>
-                        appDispatch({
-                          type: "closeSearch"
-                        })
-                      }
-                      key={post._id}
-                      to={`/post/${post._id}`}
-                      className="list-group-item list-group-item-action"
-                    >
-                      <img className="avatar-tiny" src={post.author.avatar} /> <strong>{post.title}</strong>{" "}
-                      <span className="text-muted small">
-                        by {post.author.username} on {dateFormatted}{" "}
-                      </span>
-                    </Link>
-                  )
+                  return <Post post={post} key={post._id} onClick={() => appDispatch({ type: "closeSearch" })} />
                 })}
               </div>
             )}
-            {!Boolean(state.results.length) && <p className="alert alert-danger text-center shadow-sm">Sorry, no results found for that search.</p>}
+            {!Boolean(state.results.length) && <p className="alert alert-danger text-center shadow-sm">Sorry, we could not find any results for that search.</p>}
           </div>
         </div>
       </div>
