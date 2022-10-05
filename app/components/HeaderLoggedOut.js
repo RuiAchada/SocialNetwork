941c22947a548c4dd372782dcdf277cb7d10f0a0
ruiac <rui.achada96@gmail.com>
2022-10-05 19:20:29 +0100

    section 12 registration form validations

--- app/components/HeaderLoggedOut.js
+++ app/components/HeaderLoggedOut.js
@@ -12,17 +12,14 @@ function HeaderLoggedOut(props) {
     try {
       const response = await Axios.post("/login", { username, password })
       if (response.data) {
-        /*
-        localStorage.setItem("complexappToken", response.data.token) // name, value
-        localStorage.setItem("complexappUsername", response.data.username)
-        localStorage.setItem("complexappAvatar", response.data.avatar)
-        */
         appDispatch({ type: "login", data: response.data })
+        appDispatch({ type: "flashMessage", value: "You have successfully logged in." })
       } else {
         console.log("Incorrect username / password.")
+        appDispatch({ type: "flashMessage", value: "Invalid username / password." })
       }
-    } catch (ex) {
-      console.log(ex)
+    } catch (e) {
+      console.log("There was a problem.")
     }
   }
 
